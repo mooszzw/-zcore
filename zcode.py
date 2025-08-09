591a9d5ec6543b121bf262f366b5fcf7138ea855
moos <mooszzw@outlook.com>
2025-08-09 20:42:13 +0800

    测试

--- zcode.py
+++ zcode.py
@@ -1,4 +1,19 @@
 from os import system
 with open("test.zc","r") as f:
     data = f.read()
+code = []
+for i in data:
+    line = ""
+    ifh = 0
+    if i == "{":
+        ifh = 1
+    elif i == "}":
+        line = line + i
+        ifh = 0
+        code.append(line.strip())
+        line = ""
+    if ifh == 0:
+        line = line + i
+print(code)
+        
 
