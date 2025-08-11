5093c3a52a78c7fb1c77be08f61bb311275fea5f
moos <mooszzw@outlook.com>
2025-08-11 21:13:15 +0800

    修改

--- zcode.py
+++ zcode.py
@@ -2,14 +2,21 @@ from os import system
 with open("test.zc","r") as f:
     data = f.read()
 code = []
-h = 0
+code_letters = []
 for i in data:
-    line = ""
-    if i == "{":
-        h = h+1
-    elif i == "}":
-        h=h-1
-    line = line + i
+    code_letters.append(i)
+xg = data.find("/")
+if xg != -1 and xg+1 < len(data):
+    if code_letters[xg+1] == "/":
+        while code_letters[xg]!= "\n":
+            code_letters.remove(code_letters[xg])
+            print(code_letters[xg])
+            xg = xg +1
+
+
+
+
     
-print(h)
+print(code_letters)
+
         
